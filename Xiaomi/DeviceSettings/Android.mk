ff811d7c242af1d3316f6eec2eaf747162d6ceab
aleha-druga <aleha.druga@gmail.com>
2018-08-28 08:14:01 +0000

    msm8953-common: Set LOCAL_SDK_VERSION where possible

--- Xiaomi/DeviceSettings/Android.mk
+++ Xiaomi/DeviceSettings/Android.mk
@@ -5,6 +5,7 @@ include $(CLEAR_VARS)
 	LOCAL_SRC_FILES := $(call all-java-files-under, src)
 	LOCAL_PACKAGE_NAME := DeviceSettings
 	LOCAL_CERTIFICATE := platform
+	LOCAL_PRIVATE_PLATFORM_APIS := true
 	LOCAL_PRIVILEGED_MODULE := true
 	LOCAL_PROGUARD_FLAG_FILES := proguard.flags
 include $(BUILD_PACKAGE)
