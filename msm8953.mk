730ac8da0db357d92fa7c9c019b11acf1cbd316e
aleha-druga <aleha.druga@gmail.com>
2018-12-07 15:54:33 +0000

    Revert "Handle the dalvik heap sizes dynamically to improve experience on the 3GB variant"
    This reverts commit d7ed468ddfe8529c26ba1c15c29878049e57cbdb.

--- msm8953.mk
+++ msm8953.mk
@@ -15,7 +15,6 @@
 #
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
-$(call inherit-product, frameworks/native/build/phone-xxhdpi-3072-dalvik-heap.mk)
 
 $(call inherit-product, vendor/xiaomi/msm8953-common/msm8953-common-vendor.mk)
 
